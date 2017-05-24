76b2bc42b7fe50a881e1f851a2dd13e2267aa4ba
Mentor <mentor@palokaj.co>
2017-05-24 18:35:20 +0200

    CSS fix

--- webpack.config.js
+++ webpack.config.js
@@ -142,7 +142,7 @@ module.exports = {
     },
     {
       test: /\.scss$/,
-      loaders: ["style", "css", "sass", "postcss"]
+      loaders: ["style", "css?url=false", "sass", "postcss"]
     }
     ]
   },
